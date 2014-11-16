6211e3795b9a4c87b8aad689b1d4924b50592b1d
KCJWagner <kcjwagner@gmail.com>
2014-11-16 11:11:10 -0800

    Added all the restaurants as little red dots on the map, now we need to hide them

---
+++ restaurants.js
@@ -0,0 +1,13 @@
+restauraunts = {
+	{"Cost" : 0, "Location" : [0, 0], "Cuisine" : "Indian", "Rating" : 1, "Features" : {
+		"Card" : false,
+		"Delivery" : false,
+		"Reservation" : false,
+		"Wheelchair" : false,
+		"Kids" : false,
+		"Group" : false,
+		"19+" : false,
+		"Fast" : false,
+		"Organic" : false
+	}}
+}
